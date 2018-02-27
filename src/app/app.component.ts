fb52ccffb69ceb9900ebd85181bfec62c3f9e6ae
Sean McIntyre <seanmcintyre@gmail.com>
2018-02-27 12:42:27 -0500

    typescript doesn't like using function that way

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 const round2 = (num) => Math.round((num * 100) / 100);
+const throttle = (delay, callback) => {
+  let lastCall = null;
+  return function () {
+    const time = new Date().getTime();
+    if (!lastCall || (time - lastCall) >= delay) {
+      lastCall = time;
+      callback.apply(null, arguments);
+    }
+  }
+};
 
 @Component({
   selector: 'app-root',
@@ -17,23 +27,13 @@ export class AppComponent {
     const frame = (document.querySelector('.frame') as HTMLElement);
     const logo = (document.querySelector('.logo') as HTMLElement);
     document.onmousemove = throttle(50, (e: any) => {
-        const x = round2((e.clientX / window.screen.width * 4) - 1);
-        const y = round2((e.clientY / window.screen.height * -2) + 1);
+      const x = round2((e.clientX / window.screen.width * 4) - 1);
+      const y = round2((e.clientY / window.screen.height * -2) + 1);
 
-        frame.style.setProperty('--x', `${ x + 0.01 }deg`);
-        frame.style.setProperty('--y', `${ y + 0.01 }deg`);
-        // logo.style.setProperty('--x', `${ x * 3 }px`);
-        // logo.style.setProperty('--y', `${ y * 3 }px`);
-      });
-}
-
-function throttle(delay, callback) {
-  let lastCall = null;
-  return function () {
-    const time = new Date().getTime();
-    if (!lastCall || (time - lastCall) >= delay) {
-      lastCall = time;
-      callback.apply(null, arguments);
-    }
+      frame.style.setProperty('--x', `${ x + 0.01 }deg`);
+      frame.style.setProperty('--y', `${ y + 0.01 }deg`);
+      // logo.style.setProperty('--x', `${ x * 3 }px`);
+      // logo.style.setProperty('--y', `${ y * 3 }px`);
+    });
   }
-};
+}
